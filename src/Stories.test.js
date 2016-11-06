0cbc2b599331ec17ae74018d7b260a43fdcdaf84
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-11-06 21:45:20 +0000

    test rendered output contains correct html tags and state names

--- src/Stories.test.js
+++ src/Stories.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Stories from './Stories';
+import ReactTestUtils from 'react-addons-test-utils'
+
 var sinon = require('sinon')
 
 describe("with fake server", function() {
@@ -18,9 +20,34 @@ describe("with fake server", function() {
     server.restore()
   });
 
-  it("fetches data", function () {
+  it("fetches current vote", function () {
     let jsonResponse = JSON.parse(server.responses[0].response[2])
     expect(jsonResponse.__v).toEqual(2)
   });
 
+  it("fetches Github data", function () {
+    let jsonResponse = JSON.parse(server.responses[0].response[2])
+    expect(jsonResponse.githubMetadata.title).toEqual("fakeTitle")
+  });
+
 });
+
+describe("Stories component rendering", function() {
+  var stories;
+
+  beforeEach(function(){
+     stories = ReactTestUtils.renderIntoDocument(
+        <Stories />
+      );
+  });
+
+  it("state should be empty array on first render", function() {
+      expect(stories.state.stories).toEqual([])
+  })
+
+  it("rendered html should be unordered list", function() {
+      expect(stories._reactInternalInstance._renderedComponent._tag).toEqual("ul")
+  })
+
+})
+
