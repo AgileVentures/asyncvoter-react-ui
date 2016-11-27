2cdcb632f07d64c91758e5021cf6b814d2628981
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-11-27 18:50:41 +0000

    show average vote for each story by dividing total vote count by amount of votes cast

--- src/Story.js
+++ src/Story.js
@@ -4,7 +4,7 @@ import request from 'superagent'
 class Story extends Component {
     constructor(props){
       super(props);
-      this.state = {"vote": this.props.votes};
+      this.state = {"vote": this.props.votes, "voteCount" : 0, "voteAverage": 0};
       this.sendVote = this.sendVote.bind(this)
     }
 
@@ -21,11 +21,43 @@ class Story extends Component {
       this.props.update()
     }
 
+    componentDidMount(){
+      this.fetchNumberOfVotes()
+      this.averageVote()
+    }
+
+    fetchNumberOfVotes(){
+     var that = this;
+     let storyId = this.props.story._id
+     request
+      .get('http://master.bass-seahorse-cod.app.push.drieapp.co/stories/' + storyId + "/votes")
+      .end(function(err, res){
+       that.setState({"voteCount": res.body.length})
+      })
+    }
+
+    averageVote(){
+     var that = this;
+     let storyId = this.props.story._id
+     request
+      .get('http://master.bass-seahorse-cod.app.push.drieapp.co/stories/' + storyId + "/votes")
+      .end(function(err, res){
+        let totalVotes = 0
+        let response = JSON.parse(res.text)
+        response.forEach((vote) => {
+          totalVotes+= parseInt(vote.size)
+        })
+        let average = totalVotes / response.length
+        that.setState({"voteAverage": average})
+      })
+    }
+
     render() {
       return (
         <div className="individual-story">
           <li>Title: {this.props.story.name}</li>
-          <li>Votes: {this.props.story.size} </li>
+          <li><a href={"http://master.bass-seahorse-cod.app.push.drieapp.co/stories/" + this.props.story._id + "/votes"}>Votes: {this.state.voteCount}</a></li>
+          <li>Average Vote: {this.state.voteAverage}</li>
           <li>URL: <a target='_blank' href={this.props.story.url}>{this.props.story.url}</a></li>
           <li>ID: {this.props.story._id} </li>
           <button onClick={() => this.sendVote(1)}>Vote 1 </button>
